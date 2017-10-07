f475688f42f1aa2bb3229f7811a90971734b1f79
quarsars <fatiao@in66.com>
2017-10-07 22:50:25 +0800

    refine scripts

--- my_vim_plugin/install.sh
+++ my_vim_plugin/install.sh
@@ -6,20 +6,20 @@ source ${curr}/../common.sh
 # check if ctags is installed
 ctags --version > /dev/null
 if [ $? != 0 ]; then
-	pr_info "not installed ctags" warn
+	pr_info "not installed ctags" warn verbose
 	sudo apt-get install ctags	
 else
-	pr_info "ctags already installed." info
+	pr_info "ctags already installed." info verbose
 fi
 
 quarsars_vim="quarsars.vim"
 cat ~/.vimrc | grep "source ${curr}/${quarsars_vim}" > /dev/null
 
 if [ $? != 0 ]; then
-	pr_info "not installed quarsars.vim" warn
+	pr_info "not installed quarsars.vim" warn verbose
 	echo "source ${curr}/${quarsars_vim}" >> ~/.vimrc
 else
-	pr_info "quarsars.vim already installed." info
+	pr_info "quarsars.vim already installed." info verbose
 fi
 
 git submodule update --init --recursive
